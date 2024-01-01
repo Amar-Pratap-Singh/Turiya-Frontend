a576e7182fdeb4e904de1863518ee648c7b399a2
Amar-Pratap-Singh <AmarPratap.Singh@iiitb.ac.in>
2024-01-02 00:05:21 +0530

    Updated UI issues

--- src/components/Connect.js
+++ src/components/Connect.js
@@ -40,7 +40,7 @@ export const Connect = () => {
                 <p>Join us on a path of holistic growth for a harmonious blend of body, mind, and soul enrichment.</p>
                 {/* <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p> */}
                 <Tab.Container id="projects-tabs" defaultActiveKey="first">
-                  <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
+                  <Nav variant="pills" className="nav-pills mb-3 justify-content-center align-items-center">
                     <Nav.Item>
                       <Nav.Link eventKey="first">Turiya WA Group</Nav.Link>
                     </Nav.Item>
